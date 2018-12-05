c42bf2c395ef16f21cdfac115a7c385987b9a56e
Simon Lepasteur <simon.lepasteur@swissdotnet.ch>
2018-12-05 17:47:19 +0100

    Update version to 0.5.0.

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ import os
 
 class ImmerConan(ConanFile):
     name = "immer"
-    version = "0.4.0"
+    version = "0.5.0"
     url = "https://github.com/slepasteur/conan-immer.git"
     homepage = "https://github.com/arximboldi/immer"
     author = "Simon Lepasteur <slepasteur@gmail.com>"
@@ -17,8 +17,10 @@ class ImmerConan(ConanFile):
     _source_subfolder = "source_subfolder"
     
     def source(self):
-        self.run("git clone https://github.com/arximboldi/immer")
-        os.rename("immer", self._source_subfolder)
+        source_url = "https://github.com/arximboldi/immer"
+        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version), sha256="37df55eaa7690865c588512715ac6adfbf94f13fb521d83924b5f38a264bfc34")
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
 
     def package(self):
         self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
